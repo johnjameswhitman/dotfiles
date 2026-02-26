56305ce183d60e7d6d8d2f7fc11f2379d0a8e9f4
John Whitman <john.whitman@datadoghq.com>
2026-02-26 14:47:47 +0000

    Updates installer.

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ echo "==> user: $(whoami), cwd: $(pwd)"
 
 echo "==> Installing Nix..."
 if ! command -v nix &>/dev/null; then
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
-  # Source nix in current shell
-  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --no-daemon
+  . "${HOME}/.nix-profile/etc/profile.d/nix.sh"
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+  nix-shell '<home-manager>' -A install
+  "${HOME}/.nix-profile/etc/profile.d/hm-session-vars.sh"
 else
   echo "    Nix already installed, skipping."
 fi
